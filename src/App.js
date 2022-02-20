6bb4574c22113213d0c6becf9e2175e9d4aa1c8b
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-02-20 20:20:48 +0545

    import App.scss to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 import { About, Footer, Header, Skills, Testimonials, Work } from './container'
-
 import Navbar from './components/Navbar/Navbar'
+import './App.scss'
 
 const App = () => {
   return (
