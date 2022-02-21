c274145988de0169dabe8e0c58ea701d64b374a9
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-02-21 21:45:10 +0545

    setup about section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -6,7 +6,7 @@ import './About.scss'
 
 const abouts = [
     { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
-    { title: 'Fontend Development', description: 'I am a good web developer.', imgUrl: images.about02},
+    { title: 'Frontend Development', description: 'I am a good web developer.', imgUrl: images.about02},
     { title: 'Backend Development', description: 'I am a good web developer.', imgUrl: images.about03},
     { title: 'MERN Stack', description: 'I am a good web developer.', imgUrl: images.about04},
 ]
