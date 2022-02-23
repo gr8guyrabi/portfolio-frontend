8e41731601b4f095c409bdfcc6769bbb791207af
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-02-23 15:36:16 +0545

    refactor code

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { NAV_LINKS } from '../constants'
+
 const NavigationDots = ({ active }) => {
   return (
     <div className='app__navigation'>
-        {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
+        {NAV_LINKS.map((item, index) => (
             <a
                 key={item + index}
                 href={`#${item}`} 
